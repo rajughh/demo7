b3ea107954e9609a589f4890c5c7bfc6f96fd16c
Raju Goud <121032566+rajughh@users.noreply.github.com>
2024-07-10 23:53:35 +0530

    Update read.sh

--- read.sh
+++ read.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-NAME="Raju Goud"
-echo $NAME
-
-reado
+option="${1}"
+case ${option} in
+  -f) FILE="${2}"
+     echo "file name is $FILE"
+     ;;
+  -d) DIR="${2}"
+     echo "dir name is $DIR"
+     ;;
+  *) 
+     echo "'basename ${0}' :usage: [-f file] [-d directory]"
+     exit 1
+     ;;
+esac
